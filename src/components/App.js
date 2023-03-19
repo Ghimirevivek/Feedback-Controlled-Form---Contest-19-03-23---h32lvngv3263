9633382dc5a7dffdbb75ca0f4b840919f235ab04
Ghimirevivek <108508449+Ghimirevivek@users.noreply.github.com>
2023-03-19 11:54:02 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,7 @@ const App = () => {
   const handleCommentChange = (event) => {
     setComment(event.target.value)
     if (event.target.value.length < 5) {
-      setCommentError('Comment must be at least 5 characters.')
+      setCommentError('Comment must be at least 5 characters')
     } else {
       setCommentError('')
     }
